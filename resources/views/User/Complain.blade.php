5a50ccb9f3c9e265c92066ebaaf60980dad67929
Pranav952 <pranavb952@gmail.com>
2024-02-25 20:30:21 +0545

    newchanges

--- resources/views/User/Complain.blade.php
+++ resources/views/User/Complain.blade.php
@@ -1,3 +1,65 @@
 @extends('layouts.UserNavbar')
 @section('navbar')
+<style>
+    #btn1{
+        position: relative;
+        top:4em;
+    }
+    #p{
+        font-size: 3em;
+        margin-top:1em; 
+        color: red;
+        
+    }
+    #p:hover{
+        opacity: 0.6;
+    }
+    </style>
+      
+       <center><marquee scrollamount="35"><p class="lead" id="p">Note:Your complain are strcitly monitored please donot wrtie unecessary complain</p></center></marquee>
+         <hr>
+    
+           <center><button type="button" class=" btn btn-outline-success btn-lg" data-toggle="modal" data-target="#exampleModal" id="btn1">
+            Register a Complain
+           </button>
+        </center>
+    
+
+<!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Complain</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form enctype="multipart/form-data" id="form">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" name="name" id=""> 
+            </div>
+          <form enctype="multipart/form-data" id="form">
+            <div class="form-group">
+                <label for="name">Tile</label>
+                <input type="text" class="form-control" name="title" id=""> 
+            </div>
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Problem Description</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+           </div>
+        <div class="form-group">
+            <label for="exampleFormControlFile1">Choose Image</label>
+           <input type="file" class="form-control-file" id="exampleFormControlFile1" name="image">
+          </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button id="btn" class="btn btn-success">Submit</button>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
--- resources/views/User/HomePage.blade.php
+++ resources/views/User/HomePage.blade.php
@@ -99,13 +99,13 @@
               </div>
               <div class="time-and-privacy"><time datetime="">October 14 at 1:51 PM</time></div>
             </div>
-            <button class="icon-button-2 u-margin-inline-start" aria-label="more options"><span class="icon-menu"></span></button>
+           
           </header>
           <div class="common-post-content common-content">
             <p>
              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime nihil qui dignissimos voluptatibus aperiam voluptatem ipsa illum facere sint, dolorem provident rem id beatae voluptate officia corrupti nam iste, odit quas dolorum quidem molestias et suscipit quod! Libero in beatae consequatur, velit enim quis molestias sit accusamus aliquam explicabo minus nobis aspernatur, asperiores iusto consectetur animi possimus molestiae eius aliquid. Fugit provident quos blanditiis nisi perferendis animi laudantium perspiciatis ipsa non cum, numquam debitis error suscipit aliquid soluta nobis! Saepe explicabo, recusandae, veniam odio pariatur, quia aspernatur nihil quibusdam assumenda vel sapiente praesentium atque nesciunt non eligendi ipsam quaerat harum?
             </p>   
-            <img src="/home/tilak/Desktop/Final project/images/1st.jpg"  alt="">
+          
             <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex asperiores quas tenetur vero nemo iste, soluta enim mollitia odio quaerat. Iusto, eaque! Rem magnam rerum exercitationem dolore magni minus provident.
             </p>
@@ -132,7 +132,62 @@
               </div>
               <div class="time-and-privacy"><time datetime="">October 14 at 1:51 PM</time></div>
             </div>
-            <button class="icon-button-2 u-margin-inline-start" aria-label="more options"><span class="icon-menu"></span></button>
+          </header>
+          <div class="common-post-content common-content">
+            <p>
+             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime nihil qui dignissimos voluptatibus aperiam voluptatem ipsa illum facere sint, dolorem provident rem id beatae voluptate officia corrupti nam iste, odit quas dolorum quidem molestias et suscipit quod! Libero in beatae consequatur, velit enim quis molestias sit accusamus aliquam explicabo minus nobis aspernatur, asperiores iusto consectetur animi possimus molestiae eius aliquid. Fugit provident quos blanditiis nisi perferendis animi laudantium perspiciatis ipsa non cum, numquam debitis error suscipit aliquid soluta nobis! Saepe explicabo, recusandae, veniam odio pariatur, quia aspernatur nihil quibusdam assumenda vel sapiente praesentium atque nesciunt non eligendi ipsam quaerat harum?
+            </p>   
+          </div>
+         
+          <section class="actions-buttons">
+            <ul class="actions-buttons-list u-flex">
+              <li class="actions-buttons-item"><button class="actions-buttons-button"><span class="icon">👍</span><span class="text">Up Vote</span></button></li>
+              <li class="actions-buttons-item"><button class="actions-buttons-button"><span class="icon">👎</span><span class="text">Down Vote</span></button></li>
+              <li class="actions-buttons-item"><button class="actions-buttons-button"><span class="icon">💬</span><span class="text">Comment</span></button></li>
+              <li class="actions-buttons-item"><button class="actions-buttons-button"><span class="icon">💌</span><span class="text">Share</span></button></li>
+            </ul>
+          </section>
+        </article>
+      </li>
+
+      <li class="main-feed-item">
+        <article class="common-post">
+          <header class="common-post-header u-flex">
+            <img src="https://assets.codepen.io/65740/internal/avatars/users/default.png" class="user-image" width="40" height="40" alt="">
+            <div class="common-post-info">
+              <div class="user-and-group u-flex">
+                <a href="#" target="_blank">Michael Scott</a>
+              </div>
+              <div class="time-and-privacy"><time datetime="">October 14 at 1:51 PM</time></div>
+            </div>
+          </header>
+          <div class="common-post-content common-content">
+            <p>
+             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime nihil qui dignissimos voluptatibus aperiam voluptatem ipsa illum facere sint, dolorem provident rem id beatae voluptate officia corrupti nam iste, odit quas dolorum quidem molestias et suscipit quod! Libero in beatae consequatur, velit enim quis molestias sit accusamus aliquam explicabo minus nobis aspernatur, asperiores iusto consectetur animi possimus molestiae eius aliquid. Fugit provident quos blanditiis nisi perferendis animi laudantium perspiciatis ipsa non cum, numquam debitis error suscipit aliquid soluta nobis! Saepe explicabo, recusandae, veniam odio pariatur, quia aspernatur nihil quibusdam assumenda vel sapiente praesentium atque nesciunt non eligendi ipsam quaerat harum?
+            </p>   
+          </div>
+         
+          <section class="actions-buttons">
+            <ul class="actions-buttons-list u-flex">
+              <li class="actions-buttons-item"><button class="actions-buttons-button"><span class="icon">👍</span><span class="text">Up Vote</span></button></li>
+              <li class="actions-buttons-item"><button class="actions-buttons-button"><span class="icon">👎</span><span class="text">Down Vote</span></button></li>
+              <li class="actions-buttons-item"><button class="actions-buttons-button"><span class="icon">💬</span><span class="text">Comment</span></button></li>
+              <li class="actions-buttons-item"><button class="actions-buttons-button"><span class="icon">💌</span><span class="text">Share</span></button></li>
+            </ul>
+          </section>
+        </article>
+      </li>
+
+      <li class="main-feed-item">
+        <article class="common-post">
+          <header class="common-post-header u-flex">
+            <img src="https://assets.codepen.io/65740/internal/avatars/users/default.png" class="user-image" width="40" height="40" alt="">
+            <div class="common-post-info">
+              <div class="user-and-group u-flex">
+                <a href="#" target="_blank">Michael Scott</a>
+              </div>
+              <div class="time-and-privacy"><time datetime="">October 14 at 1:51 PM</time></div>
+            </div>
           </header>
           <div class="common-post-content common-content">
             <p>
@@ -190,65 +245,6 @@
               </div>
               <div class="time-and-privacy"><time datetime="">October 14 at 1:51 PM</time></div>
             </div>
-            <button class="icon-button-2 u-margin-inline-start" aria-label="more options"><span class="icon-menu"></span></button>
-          </header>
-          <div class="common-post-content common-content">
-            <p>
-             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime nihil qui dignissimos voluptatibus aperiam voluptatem ipsa illum facere sint, dolorem provident rem id beatae voluptate officia corrupti nam iste, odit quas dolorum quidem molestias et suscipit quod! Libero in beatae consequatur, velit enim quis molestias sit accusamus aliquam explicabo minus nobis aspernatur, asperiores iusto consectetur animi possimus molestiae eius aliquid. Fugit provident quos blanditiis nisi perferendis animi laudantium perspiciatis ipsa non cum, numquam debitis error suscipit aliquid soluta nobis! Saepe explicabo, recusandae, veniam odio pariatur, quia aspernatur nihil quibusdam assumenda vel sapiente praesentium atque nesciunt non eligendi ipsam quaerat harum?
-            </p>   
-          </div>
-         
-          <section class="actions-buttons">
-            <ul class="actions-buttons-list u-flex">
-              <li class="actions-buttons-item"><button class="actions-buttons-button"><span class="icon">👍</span><span class="text">Up Vote</span></button></li>
-              <li class="actions-buttons-item"><button class="actions-buttons-button"><span class="icon">👎</span><span class="text">Down Vote</span></button></li>
-              <li class="actions-buttons-item"><button class="actions-buttons-button"><span class="icon">💬</span><span class="text">Comment</span></button></li>
-              <li class="actions-buttons-item"><button class="actions-buttons-button"><span class="icon">💌</span><span class="text">Share</span></button></li>
-            </ul>
-          </section>
-        </article>
-      </li>
-
-      <li class="main-feed-item">
-        <article class="common-post">
-          <header class="common-post-header u-flex">
-            <img src="https://assets.codepen.io/65740/internal/avatars/users/default.png" class="user-image" width="40" height="40" alt="">
-            <div class="common-post-info">
-              <div class="user-and-group u-flex">
-                <a href="#" target="_blank">Michael Scott</a>
-              </div>
-              <div class="time-and-privacy"><time datetime="">October 14 at 1:51 PM</time></div>
-            </div>
-            <button class="icon-button-2 u-margin-inline-start" aria-label="more options"><span class="icon-menu"></span></button>
-          </header>
-          <div class="common-post-content common-content">
-            <p>
-             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime nihil qui dignissimos voluptatibus aperiam voluptatem ipsa illum facere sint, dolorem provident rem id beatae voluptate officia corrupti nam iste, odit quas dolorum quidem molestias et suscipit quod! Libero in beatae consequatur, velit enim quis molestias sit accusamus aliquam explicabo minus nobis aspernatur, asperiores iusto consectetur animi possimus molestiae eius aliquid. Fugit provident quos blanditiis nisi perferendis animi laudantium perspiciatis ipsa non cum, numquam debitis error suscipit aliquid soluta nobis! Saepe explicabo, recusandae, veniam odio pariatur, quia aspernatur nihil quibusdam assumenda vel sapiente praesentium atque nesciunt non eligendi ipsam quaerat harum?
-            </p>   
-          </div>
-         
-          <section class="actions-buttons">
-            <ul class="actions-buttons-list u-flex">
-              <li class="actions-buttons-item"><button class="actions-buttons-button"><span class="icon">👍</span><span class="text">Up Vote</span></button></li>
-              <li class="actions-buttons-item"><button class="actions-buttons-button"><span class="icon">👎</span><span class="text">Down Vote</span></button></li>
-              <li class="actions-buttons-item"><button class="actions-buttons-button"><span class="icon">💬</span><span class="text">Comment</span></button></li>
-              <li class="actions-buttons-item"><button class="actions-buttons-button"><span class="icon">💌</span><span class="text">Share</span></button></li>
-            </ul>
-          </section>
-        </article>
-      </li>
-
-      <li class="main-feed-item">
-        <article class="common-post">
-          <header class="common-post-header u-flex">
-            <img src="https://assets.codepen.io/65740/internal/avatars/users/default.png" class="user-image" width="40" height="40" alt="">
-            <div class="common-post-info">
-              <div class="user-and-group u-flex">
-                <a href="#" target="_blank">Michael Scott</a>
-              </div>
-              <div class="time-and-privacy"><time datetime="">October 14 at 1:51 PM</time></div>
-            </div>
-            <button class="icon-button-2 u-margin-inline-start" aria-label="more options"><span class="icon-menu"></span></button>
           </header>
           <div class="common-post-content common-content">
             <p>
